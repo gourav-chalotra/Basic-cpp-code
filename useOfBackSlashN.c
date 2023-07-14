cc086c9a2525698f6c03e11b70e2bd74fcbb8009
Gourav Chalotra <90745717+gourav-chalotra@users.noreply.github.com>
2023-07-14 18:11:22 +0530

    Add files via upload

---
+++ useOfBackSlashN.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+    // infinite loop
+    while(1)
+    {
+        printf("Gourav");
+        sleep(1);
+        printf("\n");
+    }
+}
+
+// Video explain link :- https://youtu.be/6M1lC4W7Kzw
